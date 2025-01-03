df39c95dbc9375fc809e8276d72a09aa5daea038
vasu garg <vasugarg6656@gmail.com>
2025-01-03 22:10:33 +0530

    Updating main branch

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,8 @@ import { Application } from 'express';
 import { authenticate } from '../middlewares/authMiddleware';
 import authRoutes from './authRoutes';
 import expenseRouter from './expenseRoutes';
-import invoiceRouter from './invoiceRouter';
-import blogRouter from './blogRouter';
+import invoiceRouter from './invoiceRoutes';
+import blogRouter from './blogRoutes';
 
 export const initRoutes = (app: Application) => {
     app.use('/api/auth', authRoutes);
